f8b21c906440596f5de72600b7d4816072cc685f
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-08-15 18:47:31 +0600

    added some validation

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,17 +1,27 @@
 document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawField = document.getElementById('withdraw-field');
     const newWithdraw = parseFloat(withdrawField.value);
+
+    withdrawField.value = '';
+
+    if(isNaN(newWithdraw)){
+        alert('Please provide your amount');
+        return;
+    }
     
     const withdrawTotal = document.getElementById('withdraw-total');
     const previousWithdraw = parseFloat(withdrawTotal.innerText);
 
-    const currentWithdraw = newWithdraw+previousWithdraw;
-    withdrawTotal.innerText = currentWithdraw;
-
-    withdrawField.value = '';
-
     const balanceTotal = document.getElementById('balance-total');
     const previousBalance = parseFloat(balanceTotal.innerText);
+
+    if(newWithdraw > previousBalance){
+        alert('Tor ato takao nai');
+        return;
+    }
+
+    const currentWithdraw = newWithdraw+previousWithdraw;
+    withdrawTotal.innerText = currentWithdraw;
 
     const newBalance = previousBalance-newWithdraw;
     balanceTotal.innerText = newBalance;
